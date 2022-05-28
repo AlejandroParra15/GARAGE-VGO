06837c2842baa24fa5b6dab484fa3c098ad7f3a5
Alejandro Parra <davidalejandroparracano@gmail.com>
2022-05-27 19:18:05 -0500

    First version login and registration functions

--- app/src/main/java/com/example/garage_vgo/MainActivity.kt
+++ app/src/main/java/com/example/garage_vgo/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.example.garage_vgo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.garage_vgo.databinding.ActivityMainBinding
+import com.example.garage_vgo.databinding.DialogForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Binding initialize
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //
+        auth = FirebaseAuth.getInstance()
+        //
+        binding.btLogIn.setOnClickListener {emptyValidation()}
+
+        binding.tvCreateAccount.setOnClickListener{goRegisterActivity()}
+
+        binding.tvForgotPassword.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this)
+            val view = DialogForgotPasswordBinding.inflate(layoutInflater)
+            builder.setView(view.root)
+            val dialog = builder.create()
+            dialog.show()
+            val email = view.etForgotPassword.text
+            val btn = view.btSendEmail
+
+            btn.setOnClickListener {
+                auth.sendPasswordResetEmail(email.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Correo enviado!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+        }
     }
+
+    private fun emptyValidation(){
+        // login with username/password
+        if(binding.etUser.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()){
+            auth.signInWithEmailAndPassword(binding.etUser.text.toString(),
+                binding.etPassword.text.toString()).addOnCompleteListener(){
+                // validate if the authentication is successful
+                    if(it.isSuccessful){
+                        goHome()
+                    } else {
+                        //otherwise, display an alert
+                        showAlert()
+                    }
+                }
+        }
+    }
+
+    // Method to show an error alert when authenticating
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    // Method that directs the user to the home activity
+    private fun goHome(){
+        val homeIntent = Intent(this, HomeActivity::class.java)
+        startActivity(homeIntent)
+    }
+
+    // Method that directs the user to the home activity
+    private fun goRegisterActivity(){
+        val registerIntent = Intent(this, RegisterActivity::class.java)
+        startActivity(registerIntent)
+    }
+
 }
--- app/src/main/java/com/example/garage_vgo/RegisterActivity.kt
+++ app/src/main/java/com/example/garage_vgo/RegisterActivity.kt
@@ -1,11 +1,134 @@
 package com.example.garage_vgo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.garage_vgo.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
+
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth : FirebaseAuth
+    private lateinit var datePickerDialog : DatePickerDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        //
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //
+
+        auth = FirebaseAuth.getInstance()
+
+        //
+
+        binding.etDate.setOnClickListener {
+            val c: Calendar = Calendar.getInstance()
+            val mYear: Int = c.get(Calendar.YEAR)
+
+            val mMonth: Int = c.get(Calendar.MONTH)
+
+            val mDay: Int = c.get(Calendar.DAY_OF_MONTH)
+
+            datePickerDialog = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    binding.etDate.setText(
+                        dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year
+                    )
+                }, mYear, mMonth, mDay
+            )
+            datePickerDialog.show()
+        }
+
+        binding.btRegister.setOnClickListener{registerUser()}
     }
+
+    private fun registerUser(){
+        val email = binding.etEmailRegister.text.toString()
+        val name = binding.etName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val date = binding.etDate.text.toString()
+        val password = binding.etPasswordRegister.text.toString()
+
+        if(email.isNotEmpty() && name.isNotEmpty() && lastName.isNotEmpty() && date.isNotEmpty() && password.isNotEmpty()){
+            if(password.length >= 6){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener(){
+                    // validate if the authentication is successful
+                    if(it.isSuccessful){
+
+                        val user = auth.currentUser
+
+                        val uid = user!!.uid
+
+                        val map = hashMapOf(
+                            "nombre" to name,
+                            "apellido" to lastName,
+                            "fechanacimiento" to date,
+                            "correo" to email
+                        )
+
+                        val db = FirebaseFirestore.getInstance()
+
+                        db.collection("users").document(uid).set(map).addOnSuccessListener {
+                            goHome()
+                            Toast.makeText(this, "Usuario Registrado", Toast.LENGTH_SHORT).show()
+                        }
+                            .addOnFailureListener {
+                                showAlert("Se ha producido un error guardando la información del usuario")
+                            }
+                    } else {
+                        //otherwise, display an alert showAlert()
+                        showAlert("Se ha producido un error autenticando al usuario")
+                    }
+                }
+            }else{
+                showAlert("La contraseña debe tener 6 carácteres o más.")
+            }
+        }else{
+            showAlert("Debe llenar todos los campos del registro.")
+        }
+    }
+
+    // Method to show an error alert when authenticating
+    private fun showAlert(msg : String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(msg)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    // Method that directs the user to the home activity
+    private fun goHome(){
+        val homeIntent = Intent(this, HomeActivity::class.java)
+        startActivity(homeIntent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        } else {
+
+        }
+    }
+
+    private fun reload() {
+
+    }
+
 }
