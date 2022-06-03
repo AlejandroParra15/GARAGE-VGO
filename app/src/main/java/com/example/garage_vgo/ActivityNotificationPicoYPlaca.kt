4861d95cd6a9a2e2f966682d144deb4352e16caa
Paola Osorio <61301128+paoos9513@users.noreply.github.com>
2022-06-03 05:57:52 -0500

    Se agregaron las pantallas de las notificaciones

---
+++ app/src/main/java/com/example/garage_vgo/ActivityNotificationPicoYPlaca.kt
@@ -0,0 +1,11 @@
+package com.example.garage_vgo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityNotificationPicoYPlaca : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_pico_yplaca)
+    }
+}
---
+++ app/src/main/java/com/example/garage_vgo/KilometrajeCambioDeAceite.kt
@@ -0,0 +1,11 @@
+package com.example.garage_vgo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KilometrajeCambioDeAceite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kilometraje_cambio_de_aceite)
+    }
+}
